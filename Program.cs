06c5b1bfe780ca604de8289685d91d1156978205
Randy Chan <randychan997@gmail.com>
2020-05-06 09:50:24 -0500

    New dotnet console added.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-/**
-Name: 
-Date of completion:
 
-Please be sure to comment your code - provide a comment for each structure use in 
-program.
- */
-namespace assignment_three
+namespace assignment3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Declarations
+            Console.WriteLine("Hello World!");
         }
     }
 }
